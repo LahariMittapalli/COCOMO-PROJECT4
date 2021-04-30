716f52e5c7436617780136659f2a5b87c21af26a
yofhen <Yonathan.F.Hendrawan-1@ou.edu>
2021-04-30 08:52:11 -0500

    Updated the Basic version programs

--- COCOMOTest.java
+++ COCOMOTest.java
@@ -5,8 +5,7 @@ public class COCOMOTest {
 	static ArrayList<Double> arrEffort = new ArrayList<Double>();
 	static ArrayList<Double> arrTime = new ArrayList<Double>();
 	static ArrayList<Double> arrPersons = new ArrayList<Double>();
-	static Random rand = new Random();
-	
+		
 	// Function for rounding off float to int
 	static int fround(float x) {
 		int a;
@@ -17,119 +16,30 @@ public class COCOMOTest {
 
 	// Function to calculate parameters of Basic COCOMO
 	static void calculate(float table[][], int n, String[] mode, float size) {
-		float effort = 0f, time = 0f, staff = 0f;
+		float effort, time, staff;
 		int model = 0;
 
 		// Check the mode according to size
 		if (size >= 2 && size <= 50)
 			model = 0; // organic
+
 		else if (size > 50 && size <= 300)
 			model = 1; // semi-detached
+
 		else if (size > 300)
 			model = 2; // embedded
 
+//		System.out.println("The mode is " + mode[model]);
 
-		if (model == 0) {
-			effort = table[model][0] * (float) Math.pow(size, table[model][1]);
-			time = table[model][2] * (float) Math.pow(effort, table[model][3]);
-			staff = effort / time;
-		}
-		else if (model == 1) {
-			double EAF = 1;
-			
-			int x = rand.nextInt(5);
-			if (x == 0) EAF *= 0.75;
-			else if (x == 1) EAF *= 0.88;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 1.15;
-			else if (x == 4) EAF *= 1.4;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 0.94;
-			else if (x == 1) EAF *= 1;
-			else if (x == 2) EAF *= 1.08;
-			else if (x == 3) EAF *= 1.16;
-			x = rand.nextInt(6);
-			if (x == 0) EAF *= 0.7;
-			else if (x == 1) EAF *= 0.85;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 1.15;
-			else if (x == 4) EAF *= 1.3;
-			else if (x == 5) EAF *= 1.65;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 1;
-			else if (x == 1) EAF *= 1.11;
-			else if (x == 2) EAF *= 1.3;
-			else if (x == 3) EAF *= 1.66;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 1;
-			else if (x == 1) EAF *= 1.06;
-			else if (x == 2) EAF *= 1.21;
-			else if (x == 3) EAF *= 1.56;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 0.87;
-			else if (x == 1) EAF *= 1;
-			else if (x == 2) EAF *= 1.15;
-			else if (x == 3) EAF *= 1.3;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 0.87;
-			else if (x == 1) EAF *= 1;
-			else if (x == 2) EAF *= 1.07;
-			else if (x == 3) EAF *= 1.15;
-			x = rand.nextInt(5);
-			if (x == 0) EAF *= 1.46;
-			else if (x == 1) EAF *= 1.19;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.86;
-			else if (x == 4) EAF *= 0.71;
-			x = rand.nextInt(5);
-			if (x == 0) EAF *= 1.29;
-			else if (x == 1) EAF *= 1.13;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.91;
-			else if (x == 4) EAF *= 0.82;
-			x = rand.nextInt(5);
-			if (x == 0) EAF *= 1.42;
-			else if (x == 1) EAF *= 1.17;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.86;
-			else if (x == 4) EAF *= 0.70;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 1.21;
-			else if (x == 1) EAF *= 1.1;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.9;
-			x = rand.nextInt(4);
-			if (x == 0) EAF *= 1.14;
-			else if (x == 1) EAF *= 1.07;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.95;
-			x = rand.nextInt(5);
-			if (x == 0) EAF *= 1.24;
-			else if (x == 1) EAF *= 1.1;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.91;
-			else if (x == 4) EAF *= 0.82;
-			x = rand.nextInt(5);
-			if (x == 0) EAF *= 1.24;
-			else if (x == 1) EAF *= 1.1;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 0.91;
-			else if (x == 4) EAF *= 0.83;
-			x = rand.nextInt(5);
-			if (x == 0) EAF *= 1.23;
-			else if (x == 1) EAF *= 1.08;
-			else if (x == 2) EAF *= 1;
-			else if (x == 3) EAF *= 1.04;
-			else if (x == 4) EAF *= 1.10;
-			
-			effort = (table[model][0] * (float) Math.pow(size, table[model][1])) * (float)EAF;
-			time = table[model][2] * (float) Math.pow(effort, table[model][3]);
-			staff = effort / time;
-		}
-		else if (model == 2) {
-			System.out.println("We have not implemented the Embeded version.");
-			return;
-		}
+		// Calculate Effort
+		effort = table[model][0] * (float) Math.pow(size, table[model][1]);
+
+		// Calculate Time
+		time = table[model][2] * (float) Math.pow(effort, table[model][3]);
+
+		// Calculate Persons Required
+		staff = effort / time;
+
 		arrEffort.add((double) effort);
 		arrTime.add((double) time);
 		arrPersons.add((double) fround(staff));
@@ -138,22 +48,24 @@ public class COCOMOTest {
 	public static void main(String[] args) {
 		float table[][] = { { 2.4f, 1.05f, 2.5f, 0.38f }, { 3.0f, 1.12f, 2.5f, 0.35f }, { 3.6f, 1.20f, 2.5f, 0.32f } };
 		String mode[] = { "Organic", "Semi-Detached", "Embedded" };
+//			float size = 0.032f;
 
+//		Random rand = new Random();
 		// We use 741 as the upperbound since the graph in the paper only shows up to
 		// 10,000 Person-Months (roughly size 741)
-		int upperbound = 300 - 2;
-		int int_random = 2;
-		int numExperiment = 20000;
-		for (int i = 0; i < numExperiment; i++) {
-			int_random = rand.nextInt(upperbound) ;
-			calculate(table, 3, mode, int_random + 2);
+//		int upperbound = 741;
+//		int int_random = 0;
+		int numExperiment = 741;
+		for (int i = 2; i < numExperiment; i++) {
+//			int_random = rand.nextInt(upperbound);
+			calculate(table, 3, mode, i);
 		}
 		
 		double effort = 0, time = 0, persons = 0;
 		double totEffort = 0, totTime = 0, totPersons = 0;
 		double maxEffort = 0, maxTime = 0, maxPersons = 0;
 		double minEffort = Double.MAX_VALUE, minTime = Double.MAX_VALUE, minPersons = Double.MAX_VALUE;	
-		for(int i = 0; i<numExperiment; i++) {
+		for(int i = 0; i<numExperiment-2; i++) {
 			effort = arrEffort.get(i);
 			time = arrTime.get(i);
 			persons = arrPersons.get(i);
@@ -170,8 +82,9 @@ public class COCOMOTest {
 			totTime += time;
 			totPersons += persons;
 		}
-		System.out.println("Effort: max = " + maxEffort + ", mean = " + totEffort / arrEffort.size() + ", min = " + minEffort);
-		System.out.println("Time: max = " + maxTime + ", mean = " + totTime / arrTime.size() + ", min = " + minTime);
-		System.out.println("Persons: max = " + maxPersons + ", mean = " + totPersons / arrPersons.size() + ", min = " + minPersons);
+		System.out.println("Effort: max = " + maxEffort + ", mean = " + totEffort / numExperiment + ", min = " + minEffort);
+		System.out.println("Time: max = " + maxTime + ", mean = " + totTime / numExperiment + ", min = " + minTime);
+		System.out.println("Persons: max = " + maxPersons + ", mean = " + totPersons / numExperiment + ", min = " + minPersons);
 	}
 }
+
